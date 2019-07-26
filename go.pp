5d43c58d7f9c0ea393b5d3bb424354e10baef3af
Matt Turner <matturner@gmail.com>
2019-07-26 14:21:46 +0100

    Comment.

--- go.pp
+++ go.pp
@@ -1,3 +1,3 @@
-go get -u github.com/linuxkit/linuxkit/src/cmd/linuxkit
+go get -u github.com/linuxkit/linuxkit/src/cmd/linuxkit # is a brew tap but it's way out of date
 go get -u sigs.k8s.io/kind
 godoc
